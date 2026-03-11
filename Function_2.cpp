fd5086d072ecda276f39357c66f9cc939f709be3
Tekle, Nigist <nigist.tekle21@gmail.com>
2026-03-11 07:28:59 -0700

    code changes complete

--- Function_2.cpp
+++ Function_2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 // We will use cents for all monetary values. This will let us
 // work with integer, rather than floating-point, variables.
 const int TWINKIE_PRICE = 350;
@@ -13,7 +14,7 @@ int compute_change(int total_paid);
 
 int main()
 {
-      // Declare variables for the amount of money that the user enters,
+    // Declare variables for the amount of money that the user enters,
     // along with the change that is to be returned to them.
     int money_entered, change;
 
@@ -38,11 +39,41 @@ int main()
 
 int accept_money()
 {
-  //write your code
+    int total = 0;
+    char coin;
+
+    // Keep asking for coins until the total reaches the price
+    while (total < TWINKIE_PRICE)
+    {
+        cout << "Insert coin (d=dollar, q=quarter, m=dime, n=nickel): ";
+        cin >> coin;
+
+        if (coin == 'd')
+            total += 100;
+        else if (coin == 'q')
+            total += 25;
+        else if (coin == 'm')
+            total += 10;
+        else if (coin == 'n')
+            total += 5;
+        else
+        {
+            cout << "Invalid coin." << endl;
+            continue;
+        }
+
+        cout << "Total entered so far: $" << total/100.0 << endl;
+    }
+
+    return total;
 }
 
 int compute_change(int total_paid) 
 {
-//write your code
+    int change;
+
+    change = total_paid - TWINKIE_PRICE;
+
+    return change;
 }
 
